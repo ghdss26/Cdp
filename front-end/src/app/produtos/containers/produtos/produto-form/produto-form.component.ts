80d2de5ee4fb6fbead0ea89f7ce84ded9142e8d1
ghdss26 <gustavo902@hotmail.com>
2023-02-03 10:33:15 -0300

    Validação de Formulários

--- front-end/src/app/produtos/containers/produtos/produto-form/produto-form.component.ts
+++ front-end/src/app/produtos/containers/produtos/produto-form/produto-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Produto } from 'src/app/produtos/model/produto';
@@ -16,7 +16,9 @@ export class ProdutoFormComponent implements OnInit{
 
   form = this.NonNullableFormBuilder.group({
     id: [''],
-    titulo:[''],
+    titulo:['', [Validators.required,
+    Validators.minLength(5),
+    Validators.maxLength(100)]],
     preco: 0,
     estoque: 0
   });
@@ -63,5 +65,31 @@ export class ProdutoFormComponent implements OnInit{
   private onError() {
 
     this.snackBar.open('Erro ao salvar produto', '', {duration: 5000});
+  }
+
+  getErrorMessage(fieldName: string) {
+
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+
+      return 'Campo Obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+
+      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres`;
+    }
+
+    if (field?.hasError('maxlength')) {
+
+      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 100
+      return `Tamanho máximo precisa ser de ${requiredLength} caracteres`;
+    }
+
+
+    return 'Campo Inválido';
+
   }
 }
